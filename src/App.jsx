94854a11221e19c18c1e119ff01d46d70b4161aa
pratikpatel30 <pratikpatel3060@gmai.com>
2023-10-20 15:00:30 +0530

    create three use state and create one function for frtch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './index.css'
 import Navbar from './assets/components/Navbar'
@@ -7,7 +7,24 @@ import MovieCard from './assets/components/MovieCard'
 
 
 function App() {
-  
+    //crete three state
+    const [allMovieData, setAllMovieData] = useState9([]);
+    const [searchMovie, setSearchMovie] = useState('');
+    const [loading, setLoading] = useState(false);
+    
+    // create function for fetch the data
+    const fetchMovieData = async () => {
+      try {
+            setLoading(true);
+            const res = await fetch(`http://www.omdbapi.com/?i=${searchMovie}&apikey=1a28bd87`);
+            const data = await res.json();
+            setAllMovieData(data.Search);
+            setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      }
+    }
 
   return (
     <>
