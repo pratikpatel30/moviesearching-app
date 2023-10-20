eb56295b19f99eadb26e47e6bbb9cbbf6469bc65
pratikpatel30 <pratikpatel3060@gmai.com>
2023-10-20 14:26:55 +0530

    Creted Searchbox

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    <div>SearchBar</div>
+    <div>
+        <div className='input flex justify-center px-5 lg:px-0 py-5'>
+            <input 
+            type="text"
+            placeholder='Search here'
+            className='shadow-md bg-gray-200 placeholder-gray-400 rounded-l-lg px-2 py-2 w-80 outline-none boeder-2 border-gray-500 text-black '
+            />
+            <button
+            className='bg-[#40407a] px-4 rounded-r-lg text-white shadow-md border-b-2 border-r-2 border-t-2 border-gray-500'
+            >Search</button>
+        </div>
+    </div>
   )
 }
 
